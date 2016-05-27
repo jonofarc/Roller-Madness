edd0fce8878c3201e0074c2bbb9e6dafc22f88ed
unknown <jmaldonadom1989@gmail.com>
2016-05-27 13:24:17 -0600

    added touchcontrols for mobile devices

---
+++ Assets/Scripts/ControlChange.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ControlChange : MonoBehaviour {
+	public bool Gyroscope=true;
+	public GameObject ControlType1;
+	public GameObject ControlType2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void ToogleControls(){
+	
+		Gyroscope = !Gyroscope;
+		if (Gyroscope) {
+			this.gameObject.GetComponent<Text> ().text = "Giroscopio";
+			ControlType2.SetActive (false);
+			ControlType1.SetActive (true);
+
+		} else {
+			this.gameObject.GetComponent<Text> ().text = "Joystick";
+			ControlType1.SetActive (false);
+			ControlType2.SetActive (true);
+		}
+	
+	}
+}
