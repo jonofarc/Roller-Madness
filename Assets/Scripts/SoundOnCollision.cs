07ccf53d42aba93b802b632d608a8dba95c7ae23
unknown <jmaldonadom1989@gmail.com>
2016-06-01 13:55:24 -0600

    Sound efect for enemy explosions added, added Main menu music, changed sound efect for custom ones

---
+++ Assets/Scripts/SoundOnCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundOnCollision : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision coll) {
+		
+		if (coll.gameObject.tag=="Player") {
+			GetComponent<AudioSource>().Play();
+		}
+
+
+	}
+}
